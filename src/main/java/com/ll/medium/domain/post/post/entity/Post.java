9996a9131ee78cb768d0d5c971ff0a017ec0c75a
T189216 <tus0926@naver.com>
2024-07-03 17:57:39 +0900

    feat: 조회수

--- src/main/java/com/ll/medium/domain/post/post/entity/Post.java
+++ src/main/java/com/ll/medium/domain/post/post/entity/Post.java
@@ -1,15 +1,23 @@
 package com.ll.medium.domain.post.post.entity;
 
 import com.ll.medium.domain.member.member.entity.Member;
+import com.ll.medium.domain.post.post.postLike.entity.PostLike;
 import com.ll.medium.global.jpa.BaseEntity;
 import jakarta.persistence.Entity;
 import jakarta.persistence.FetchType;
 import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
 import lombok.*;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import static jakarta.persistence.CascadeType.ALL;
+import static lombok.AccessLevel.PROTECTED;
+
 @Entity
-@NoArgsConstructor(access = AccessLevel.PROTECTED)
-@AllArgsConstructor(access = AccessLevel.PROTECTED)
+@NoArgsConstructor(access = PROTECTED)
+@AllArgsConstructor(access = PROTECTED)
 @Builder
 @Getter
 @Setter
@@ -22,4 +30,11 @@ public class Post extends BaseEntity {
     private String body;
 
     private boolean isPublished;
+
+    @Setter(PROTECTED)
+    private long hit;
+
+    public void increaseHit() {
+        hit++;
+    }
 }
