4724e5b1ed755e13bb88b505dd1e70e19c56383f
T189216 <tus0926@naver.com>
2024-07-03 17:45:38 +0900

    feat: 멤버십 레벨

--- src/main/java/com/ll/medium/domain/member/member/entity/Member.java
+++ src/main/java/com/ll/medium/domain/member/member/entity/Member.java
@@ -6,6 +6,7 @@ import jakarta.persistence.Transient;
 import lombok.*;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.parameters.P;
 
 import java.util.ArrayList;
 import java.util.Collection;
@@ -19,14 +20,18 @@ import java.util.List;
 @Setter
 public class Member extends BaseEntity {
     private String username;
-
     private String password;
+    private int membershipLevel;
 
     @Transient
     public Collection<? extends GrantedAuthority> getAuthorities() {
         List<GrantedAuthority> authorities = new ArrayList<>();
 
         authorities.add(new SimpleGrantedAuthority("ROLE_MEMBER"));
+
+        if (membershipLevel > 0) {
+            authorities.add(new SimpleGrantedAuthority("ROLE_PAID"));
+            }
 
         if (List.of("system", "admin").contains(username)) {
             authorities.add(new SimpleGrantedAuthority("ROLE_ADMIN"));
--- src/main/java/com/ll/medium/domain/member/member/service/MemberService.java
+++ src/main/java/com/ll/medium/domain/member/member/service/MemberService.java
@@ -4,6 +4,8 @@ import com.ll.medium.domain.member.member.entity.Member;
 import com.ll.medium.domain.member.member.repository.MemberRepository;
 import com.ll.medium.global.rsData.RsData.RsData;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -38,5 +40,9 @@ public class MemberService {
 
     public long count() {
         return memberRepository.count();
+    }
+
+    public Page<Member> search(String kw, Pageable pageable) {
+        return memberRepository.findByUsernameContainsIgnoreCase(kw, pageable);
     }
 }
